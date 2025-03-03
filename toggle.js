a61d360521522776c776cb2607652f22ba2c59af
Andy Liu <aandyliu46@gmail.com>
2025-03-02 22:00:41 -0800

    cached theme using localStorage api

--- toggle.js
+++ toggle.js
@@ -1,12 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
-
-    // toggle light and dark mode...
+document.addEventListener("DOMContentLoaded", function () {
     const themeToggle = document.getElementById("theme");
-    console.log(themeToggle);
-    let darkMode = false;
-    themeToggle.addEventListener('click', () => {
-        darkMode = !darkMode;
-        document.body.className = darkMode ? 'dark-mode' : 'light-mode';
-      });
 
+    let darkMode = localStorage.getItem('theme') === 'dark';
+
+    document.body.className = darkMode ? 'dark-mode' : 'light-mode';
+
+    themeToggle.addEventListener('click', () => {
+        darkMode = !darkMode; 
+        document.body.className = darkMode ? 'dark-mode' : 'light-mode';
+        
+        localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+    });
 });
