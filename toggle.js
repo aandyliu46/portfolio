b7db963c5e523a7c1579db5b2f58918f761c314c
Andy Liu <aandyliu46@gmail.com>
2025-03-02 22:55:10 -0800

    fixed small bug with the toggle

--- toggle.js
+++ toggle.js
@@ -4,11 +4,13 @@ document.addEventListener("DOMContentLoaded", function () {
     let darkMode = localStorage.getItem('theme') === 'dark';
 
     document.body.className = darkMode ? 'dark-mode' : 'light-mode';
-
+    document.body.classList.add('theme-loaded');
     themeToggle.addEventListener('click', () => {
         darkMode = !darkMode; 
         document.body.className = darkMode ? 'dark-mode' : 'light-mode';
         
         localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+        document.body.classList.add('theme-loaded');
+
     });
 });
