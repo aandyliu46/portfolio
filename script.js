5f558add6660371d6265b5efb160d7a3b85b0f99
aandyliu46 <aandyliu46@gmail.com>
2025-03-16 16:04:15 -0700

    addded local data, then parsing it into a JS object

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 window.addEventListener('DOMContentLoaded', function(){
 
+    initLocalData()
     this.document.getElementById("localButton").addEventListener('click', function () { sendRequest});
     
     this.document.getElementById("remoteButton").addEventListener('click', function () {sendRequest});
 
 });
+
+function initLocalData() {
+    fetch('db.json')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(projectData => {
+            localStorage.setItem(projectData, JSON.stringify(projectData))
+        })
+}
