b6026ad1d003ac7def8c35ac30af6966c773d041
aandyliu46 <aandyliu46@gmail.com>
2025-03-15 17:03:46 -0700

    added basic styling to ProjectCard

--- project-card-component.js
+++ project-card-component.js
@@ -1,3 +1,19 @@
 class ProjectCard extends HTMLElement {
-    
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        const style = `
+            border: .25rem solid black;
+            margin: auto;
+            padding: 2rem;
+            text-align: center;
+            width: 50vw;
+        `;
+        
+        this.innerHTML= ''
+    }
 }
+
+CustomElementRegistry.define('project-card', ProjectCard)
