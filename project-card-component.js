0f094fe64ff3903d927f02ab8bffda7c71cff98b
aandyliu46 <aandyliu46@gmail.com>
2025-03-15 20:49:06 -0700

    added html to ProjectCard

--- project-card-component.js
+++ project-card-component.js
@@ -4,7 +4,13 @@ class ProjectCard extends HTMLElement {
     }
 
     connectedCallback(){
+        // remember to add flex to the style.
         const style = `
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            width: fit-content;
             border: .25rem solid black;
             margin: auto;
             padding: 2rem;
@@ -12,7 +18,18 @@ class ProjectCard extends HTMLElement {
             width: 50vw;
         `;
         
-        this.innerHTML= ''
+        const h3Style =`
+            font-size: 1.3rem;
+        `;
+
+        this.innerHTML= `
+            <div style="${style}"> 
+            <h3 style="${h3Style}"> Project: </h3>
+            <picture> </picture>
+            <p> </p>
+            <a href = >
+            </div>
+            `
     }
 }
 
